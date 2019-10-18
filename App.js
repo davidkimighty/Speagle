0fcd04aaf1fe2a6bf7b87eda98a0e4237e7cc266
HyunnnnKim <hwkim931224@gmail.com>
2019-10-18 13:07:30 +0900

    adding image

--- App.js
+++ App.js
@@ -1,26 +1,16 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import { Image } from 'react-native';
+import { View, Image } from 'react-native';
 
-export default class HelloWorldApp extends Component {
+export default class Bananas extends Component {
   render() {
     let pic = {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
     return (
-      <View style={styles.container}>
-        <Text>Hello, world!</Text>
-                <Image source={pic} style={{width: 193, height: 110}}/>
+      <View style={{alignItems: 'center', top: 150}}>
+        <Image source={pic} style={{width: 193, height: 110}}/>
       </View>
+      
     );
   }
 }
-
-const styles = StyleSheet.create(
-  {
-       container:
-       {
-           flex: 1,
-           backgroundColor: '#272735' // Set your own custom Color
-       }
-  });
