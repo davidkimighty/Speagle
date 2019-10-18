14995b9fa01f525d124f059c1fc908b0b48a541f
HyunnnnKim <hwkim931224@gmail.com>
2019-10-18 14:01:32 +0900

    understanding props

--- App.js
+++ App.js
@@ -1,16 +1,28 @@
 import React, { Component } from 'react';
-import { View, Image } from 'react-native';
-
-export default class Bananas extends Component {
+import { AppRegistry, Text, View, Image } from 'react-native';
+// We are making our custom component to make use of it everywhere we want 
+class SayHi extends Component {
   render() {
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
     return (
-      <View style={{alignItems: 'center', top: 150}}>
-        <Image source={pic} style={{width: 193, height: 110}}/>
+      <View>
+        <Text>Hello, {this.props.firstname} {this.props.lastname}</Text>
+        <Image
+          style={{width: 50, height: 50}}
+          source={this.props.image}
+        />
       </View>
-      
+    );
+  }
+}
+export default class GreetingPeople extends Component {
+  render() {
+    let picture = { uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' };
+    return (
+      <View style={{justifyContent: 'center',alignItems: 'center', flex:1}}>
+        <SayHi firstname='Hyunwoo' lastname='Kim' />
+        <SayHi firstname='Dapea' lastname='Mo' />
+        <SayHi image={picture} />
+      </View>
     );
   }
 }
