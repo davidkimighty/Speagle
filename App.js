e17bbcec72ed4d414619efbbc90f674052cbb653
HyunnnnKim <hwkim931224@gmail.com>
2019-10-21 15:40:59 +0900

    understanding flex
    and some rearranging

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View, Image, StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
-});
-
 class SayHi extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.body}>
         <Text>Hello, {this.props.firstname} {this.props.lastname}</Text>
       </View>
     );
@@ -27,7 +16,11 @@ class ShowImage extends Component {
     return (
       <View>
         <Image
-          style={{width: 100, height: 100}}
+          style={{
+            width: 200,
+            height: 150,
+            borderRadius: 20
+          }}
           source={this.props.image}
         />
       </View>
@@ -37,23 +30,28 @@ class ShowImage extends Component {
 
 class Blink extends Component {
   componentDidMount(){
-    // Toggle the state every second
     setInterval(() => (
       this.setState(previousState => (
         { isShowingText: !previousState.isShowingText }
       ))
     ), 1000);
   }
-  //state object
   state = { isShowingText: true };
 
   render() {
-    if (!this.state.isShowingText) return null;
-    return (
-      <View>
-        <Text style={[styles.bigBlue, styles.red]}>{this.props.text}</Text>
-      </View> 
-    );
+    if (!this.state.isShowingText){
+      return (
+        <View>
+          <Text style={styles.red}>{this.props.text}</Text>
+        </View> 
+      );
+    } else{
+      return (
+        <View>
+          <Text style={styles.blue}>{this.props.text}</Text>
+        </View> 
+      );
+    }
   }
 }
 
@@ -61,12 +59,78 @@ export default class GreetingPeople extends Component {
   render() {
     let picture = { uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' };
     return (
-      <View style={{justifyContent: 'center',alignItems: 'center', flex:1, backgroundColor: '#6ED4C8'}}>
-        <SayHi firstname='Hyunwoo' lastname='Kim' />
-        <SayHi firstname='Dapea' lastname='Mo' />
-        <ShowImage image={picture} />
-        <Blink text='Blink Test' />
+      <View style={styles.container}>
+        <View style={styles.firstrow}>
+          <Text style={styles.title}>Flex</Text>
+        </View>
+        <View style={styles.secondrow}>
+          <SayHi firstname='Hyunwoo' lastname='Kim' />
+          <SayHi firstname='Dapea' lastname='Mo' />
+        </View>
+        <View style={styles.thirdrow}>
+          <Blink text='Blink Test' />
+        </View>
+        <View style={styles.forthrow}>
+          <ShowImage image={picture} />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 30,
+    margin: 40,
+    backgroundColor: 'white'
+  },
+  firstrow: {
+    flex: 1,
+    backgroundColor: "white",
+    justifyContent: 'center', 
+    alignItems: 'center' 
+  },
+  secondrow: {
+    flex: 1,
+    backgroundColor: "#ffc857",
+    justifyContent: 'center', 
+    alignItems: 'center',
+    borderTopStartRadius: 20,
+    borderTopEndRadius: 20
+  },
+  thirdrow: {
+    flex: 2,
+    backgroundColor: "#4b3f72",
+    justifyContent: 'center', 
+    alignItems: 'center' 
+  },
+  forthrow: {
+    flex: 3,
+    backgroundColor: "#119da4",
+    justifyContent: 'center', 
+    alignItems: 'center',
+    borderBottomStartRadius: 20,
+    borderBottomEndRadius: 20
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 35,
+    textAlignVertical: 'center',
+    textAlign: 'center'
+  },
+  body: {
+    color: 'black',
+    fontSize: 20
+  },
+  red: {
+    fontWeight: 'bold',
+    color: 'red',
+    fontSize: 40
+  },
+  blue: {
+    fontWeight: 'bold',
+    color: 'blue',
+    fontSize: 40
+  }
+});
